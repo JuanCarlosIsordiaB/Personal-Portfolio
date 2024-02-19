540730dcec5aa7cd80b2cbc6f3dac52760e0d4f0
JuanCarlosIsordiaB <juancarlosisordiab@gmail.com>
2024-02-18 19:20:27 -0600

    img done

--- src/components/WorkItem.jsx
+++ src/components/WorkItem.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const WorkItem = ({ title, imgURL, tech, link }) => {
   return (
     <div className="bg-slate-300 dark:bg-slate-800 my-2 rounded-lg overflow-hidden">
+      <img src='https://github.com/JuanCarlosIsordiaB/Personal-Portfolio/blob/main/src/img/Taskify.png'className="w-full h-auto" />
       <img src={imgURL} alt={title} className="w-full h-auto" />
       <div className="text-gray-600 dark:text-gray-300 p-5 w-full">
         <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold">
