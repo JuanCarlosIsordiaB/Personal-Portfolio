d7dd0b2a63c4fa40b59c74fc65cb26d17cb61984
JuanCarlosIsordiaB <juancarlosisordiab@gmail.com>
2024-02-18 19:16:30 -0600

    img done

--- src/data/worksData.jsx
+++ src/data/worksData.jsx
@@ -3,19 +3,19 @@ export default [
       title: "Taskify",
       imgURL: `../src/img/Taskify.png`,
       tech: ["React.js", "Tailwind", "Node.js", "Express.js", "MongoDB"],
-      link: 'https://taskify-app-frontend.vercel.app'
+      link: 'https://github.com/JuanCarlosIsordiaB/Personal-Portfolio/blob/main/src/img/Taskify.png'
     },
     {
       title: "CryptoApp",
       imgURL: '../src/img/CryptoApp.png',
       tech: ["React.js", "Tailwind"],
-      link: 'https://crypto-app-react-kohl.vercel.app/'
+      link: 'https://github.com/JuanCarlosIsordiaB/Personal-Portfolio/blob/main/src/img/CryptoApp.png'
     },
     {
       title: "Patients Administrator",
       imgURL: '../src/img/PatientsAdministrator.png',
       tech: ["React.js", "Tailwind"],
-      link: 'https://patients-admnistrator-react.vercel.app/'
+      link: 'https://github.com/JuanCarlosIsordiaB/Personal-Portfolio/blob/main/src/img/PatientsAdministrator.png'
     },
   ];
   
