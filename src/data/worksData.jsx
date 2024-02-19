5f72ab895322854d9740ea36d933964cfde14365
JuanCarlosIsordiaB <juancarlosisordiab@gmail.com>
2024-02-18 20:31:55 -0600

    img done

--- src/data/worksData.jsx
+++ src/data/worksData.jsx
@@ -1,19 +1,19 @@
 export default [
     {
       title: "Taskify",
-      imgURL: '../images/Taskify.png',
+      imgURL: '../images/Task.jpg',
       tech: ["React.js", "Tailwind", "Node.js", "Express.js", "MongoDB"],
       link: 'https://taskify-app-frontend.vercel.app'
     },
     {
       title: "CryptoApp",
-      imgURL: `../images/CryptoApp.png`,
+      imgURL: `../images/Crypto.jpg`,
       tech: ["React.js", "Tailwind"],
       link: 'https://crypto-app-react-kohl.vercel.app/'
     },
     {
       title: "Patients Administrator",
-      imgURL: `../images/PatientsAdministrator.png`,
+      imgURL: `../images/Admin.jpg`,
       tech: ["React.js", "Tailwind"],
       link: 'https://patients-admnistrator-react.vercel.app/'
     },
